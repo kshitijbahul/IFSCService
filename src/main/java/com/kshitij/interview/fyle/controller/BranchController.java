fc5f32e1d00f9ca41d38e9a5bb06d453bd5bdc54
KshitijBahul <chunks.3@gmail.com>
2019-07-22 13:04:17 +0530

    Added Loggers

--- src/main/java/com/kshitij/interview/fyle/controller/BranchController.java
+++ src/main/java/com/kshitij/interview/fyle/controller/BranchController.java
@@ -2,6 +2,7 @@ package com.kshitij.interview.fyle.controller;
 
 import com.kshitij.interview.fyle.entities.Branch;
 import com.kshitij.interview.fyle.service.BranchService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class BranchController {
     private BranchService branchService;
 
@@ -21,11 +23,13 @@ public class BranchController {
 
     @GetMapping("/branch/{code}")
     private Branch getBranchByIfsc(@PathVariable("code") String ifsccode){
+        log.info("Entered the /branch/ controller ");
         return branchService.getBranchByIFSC(ifsccode);
     }
 
     @GetMapping("/branches/bank/{bank}/city/{city}")
     private List<Branch> getBranchesByBankAndCity(@PathVariable("bank") String bank, @PathVariable("city") String city){
+        log.info("Entered the get branch by city controller ");
         return branchService.getBranchesByBankNameAndCity(bank,city);
     }
 }
--- src/main/java/com/kshitij/interview/fyle/service/BranchService.java
+++ src/main/java/com/kshitij/interview/fyle/service/BranchService.java
@@ -3,10 +3,12 @@ package com.kshitij.interview.fyle.service;
 import com.kshitij.interview.fyle.entities.Branch;
 import com.kshitij.interview.fyle.exceptions.BranchNotFoundException;
 import com.kshitij.interview.fyle.repository.BranchRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@Slf4j
 public class BranchService {
     private BranchRepository branchRepository;
     public BranchService(BranchRepository branchRepository){
@@ -14,10 +16,12 @@ public class BranchService {
     }
 
     public Branch getBranchByIFSC(String ifscCode) {
+        log.info("returning Bank details with searched with ifscCode");
         return branchRepository.getBranchByIfsc(ifscCode).orElseThrow(() -> new BranchNotFoundException("Branch not found for "+ifscCode));
     }
 
     public List<Branch> getBranchesByBankNameAndCity(String bank, String city) {
+        log.info("returning All Branches for "+bank+" and city "+city);
         return branchRepository.getBranchesByBankNameAndCity(bank,city);
     }
 }
